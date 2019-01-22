feafd12192c3d74cf6375d678ab87e0eaf28224f
AnnaPytlik <annapytlik07@gmail.com>
2019-01-22 11:03:00 +0100

    init

---
+++ online-store/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" class="container my-5">
+    <h1>My online store</h1>
+    <div class="row">
+      <Item
+        v-for="item in forSale"
+        :key="item.invId"
+        :invId="item.invId"
+        :name="item.name"
+        :image="item.image"
+        :price="item.price" />
+    </div>
+ {{ inCart }}
+  </div>
+</template>
+<script>
+import Item from './Item.vue';
+export default {
+  name: 'app',
+  computed: {
+    forSale() { return this.$store.getters.forSale; },
+    inCart() { return this.$store.getters.inCart; },
+  },
+  components: {
+    Item,
+  },
+};
+</script>
+<style>
+  /* Nothing for now */
+</style>
